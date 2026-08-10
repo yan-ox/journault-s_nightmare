ff85dfed0755749a24116b42c6d809d37dfb6ccc
yan-ox <yanuxdyno@gmail.com>
2026-08-10 10:44:39 +0200

    yes

--- listes.ml
+++ listes.ml
@@ -19,10 +19,30 @@ let miroir (l : 'a list): 'a list =
     |[] -> lf
   in miraux l []
 
-let miroir2 (l : 'a list): 'a list =
+let miroir2 (l : 'a list): 'a list = 
+  let addup (l2 : 'a list) (x : 'a) : 'a list = x :: l2 
+  in List.fold_left (addup) [] l
+
+let rec conc (l1 : 'a list) (l2 : 'a list) : 'a list =
+  match l1 with
+  |x :: tl -> x :: (conc tl l2)
+  |[] -> l2 
   
+let rec conc2 (l1 : 'a list) (l2 : 'a list) : 'a list =
+  let addup (l3 : 'a list) (x : 'a) : 'a list = x :: l3
+  in List.fold_left (addup) l1 (miroir l2)
+  
+let rec thanos_snap (l : 'a list) (x : 'a) : 'a list =
+  match l with
+  |a :: tl -> if a = x then thanos_snap tl x else a :: thanos_snap tl x
+  |[] -> []
 
 let test0 = 3 :: 4 :: 67 :: 32 :: 90 :: 42 :: []
+let test2 = 3 :: 4 :: 67 :: 32 :: 90 :: 42 :: []
 let () = assert(der_el test0 = 42)
 let () = assert(der_el2 test0 = Some 42)
 let () = assert(miroir test0 = 42 :: 90 :: 32 :: 67 :: 4 :: 3 :: [])
+let () = assert(miroir2 test0 = 42 :: 90 :: 32 :: 67 :: 4 :: 3 :: [])
+let () = assert(conc test0 test2 = 3 :: 4 :: 67 :: 32 :: 90 :: 42 :: 3 :: 4 :: 67 :: 32 :: 90 :: 42 :: [])
+let () = assert(conc2 test0 test2 = 3 :: 4 :: 67 :: 32 :: 90 :: 42 :: 3 :: 4 :: 67 :: 32 :: 90 :: 42 :: [])
+let () = assert(thanos_snap test0 42 = 3 :: 4 :: 67 :: 32 :: 90 :: [])

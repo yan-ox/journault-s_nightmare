f1afbb375ced9848e4db9f44247a2d05039802e9
yan-ox <yanuxdyno@gmail.com>
2026-08-18 14:23:45 +0200

    R.2-1,2-2

--- tableaux.ml
+++ tableaux.ml
@@ -1,5 +1,22 @@
 (*R.2-1*)
+let prem_zero (tab : int array): int =
+    let n = Array.length tab in
+    let i = ref 0 in
+    while(!i < n && tab.(!i) <> 0) do
+        incr i
+    done;
+    if (!i >= n) then -1 else !i
 (*R.2-2*)
+exception Found of int
+let prem_zero2 (tab : int array) : int =
+    let n = Array.length tab in
+    try
+        for i = 0 to (n-1) do
+            if tab.(i) = 0 then raise (Found(i))
+        done; -1
+    with
+        Found(i)-> i
+
 (*R.2-3*)
 (*R.2-4*)
 (*R.2-5*)

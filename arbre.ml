e53b9fb40ff0404130cbd7047ca75caa782dee16
yan-ox <yanuxdyno@gmail.com>
2026-08-27 17:39:11 +0200

    R.3-1 R.3-2

--- arbre.ml
+++ arbre.ml
@@ -4,3 +4,23 @@ type 'a btree =
 
 type 'a gtree =
     |GN of 'a * 'a gtree list
+
+(*R.3-1*)
+let est_videb (ab : 'a btree): bool =
+    match ab with
+        |E -> true
+        |_ -> false
+(*R.3-2*)
+let hautb (ab : 'a btree): int =
+    let rec haux (ab : 'a btree) (acc : int): int =
+        match ab with
+            |E -> acc
+            |N(_, g, d) -> max (haux g (acc + 1)) (haux d (acc + 1))
+    in haux ab (-1)
+(*R.3-3*)
+(*R.3-6*)
+(*R.3-7*)
+(*R.3-8*)
+(*R.3-15*)
+(*R.3-18*)
+(*R.3-24*)

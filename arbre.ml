dc63a0215d840966417d76d14dc24ec7c5074e4a
yan-ox <yanuxdyno@gmail.com>
2026-08-27 18:35:56 +0200

    R.3-3 R.3-6 R.3-7

--- arbre.ml
+++ arbre.ml
@@ -18,9 +18,39 @@ let hautb (ab : 'a btree): int =
             |N(_, g, d) -> max (haux g (acc + 1)) (haux d (acc + 1))
     in haux ab (-1)
 (*R.3-3*)
+let tailleg (ag : 'a gtree): int =
+    let rec taux (ag : 'a gtree) (acc : int) : int =
+        match ag with
+            |GN(_, lag) -> listaux lag (acc + 1)
+    and listaux (lag : 'a gtree list) (acc : int): int =
+        match lag with
+            |ag :: tlag -> listaux tlag (taux ag (acc))
+            |_  -> acc
+    in taux ag 0
 (*R.3-6*)
+let rec est_present (ab : 'a btree) (x : 'a): bool =
+    match ab with
+        |E -> false
+        |N(a, g, d) -> if a = x then true else est_present g x || est_present d x
 (*R.3-7*)
+let rec minmax (ab : 'a btree): (int*int) option =
+    match ab with
+        |N(x, g, d) -> begin
+            let g2, d2 = minmax g, minmax d in 
+            match g2, d2 with
+            |Some (mi, ma), Some (mi1, ma1) -> let mi2, ma2 = min mi mi1, max ma ma1 in Some (min mi2 x, max ma2 x)
+            |Some (mi, ma), None -> Some (min mi x, max ma x)
+            |None, Some (mi, ma) -> Some (min mi x, max ma x)
+            |None, None -> Some(x, x)
+        end
+        |E -> None
 (*R.3-8*)
 (*R.3-15*)
 (*R.3-18*)
 (*R.3-24*)
+
+let ag = GN(1, [GN(2, [GN(4, [])]); GN(3, [])])
+let ab = N(1, N(2, N(4, E, E), E), N(3, E, E))
+let () = assert(tailleg ag = 4)
+let () = assert(est_present ab 3)
+let () = assert(minmax ab = Some(1, 4))
